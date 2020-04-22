a3721048a42d4118faf48cc54fa81bcb45675739
Marcel Kolarcik <60097861+marcelkolarcik@users.noreply.github.com>
2020-04-22 10:33:24 +0100

    update run.py, requirements.txt

--- run.py
+++ run.py
@@ -1,7 +1,17 @@
+import os
 from flask import Flask
-app = Flask(__name__)
 
 """testing"""
+app = Flask(__name__)
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def hello():
+    return "Hello World from Globtopus"
+
+
+"""it should work"""
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
