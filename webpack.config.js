b61466fd1941f608978a65d627bcd490f2a5cab1
Marcel Kolarcik <60097861+marcelkolarcik@users.noreply.github.com>
2020-04-22 12:54:10 +0100

    install webpack, cypress, created initial dir structure

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+/*
+ *
+ * AS I NEEDED TO OUTPUT DIFFERENT JS FILES TO DIFFERENT HTML PAGES*
+ * FILES, I FOUND SOLUTION AT
+ *
+ *
+ * https://stackoverflow.com/questions/35903246/how-to-create-multiple-output-paths-in-webpack-config
+ */
+const path = require ( 'path' );
+
+var config = {
+	
+	module : {}
+};
+
+var index = Object.assign ( {}, config, {
+	
+	entry  : [
+		'./static/src/js/index.js',
+
+	],
+	output : {
+		path     : path.resolve ( __dirname, 'static/dist/js' ),
+		filename : "index.js"
+	},
+	watch  : true
+} );
+var user = Object.assign ( {}, config, {
+	
+	entry  : [
+		'./static/src/js/user.js',
+	],
+	output : {
+		path     : path.resolve ( __dirname, 'static/dist/js' ),
+		filename : "user.js"
+	},
+	watch  : true
+} );
+
+var admin = Object.assign ( {}, config, {
+	
+	entry  : [
+		'./static/src/js/user.js',
+	
+	],
+	output : {
+		path     : path.resolve ( __dirname, 'static/dist/js' ),
+		filename : "admin.js"
+	},
+	watch  : true
+} );
+
+
+
+
+// Return Array of Configurations
+module.exports = [
+	index, user, admin
+
+];
