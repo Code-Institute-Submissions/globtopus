ba41485d8ad7cd64f8058e3150a47a1d47535109
Marcel Kolarcik <60097861+marcelkolarcik@users.noreply.github.com>
2020-04-23 12:37:16 +0100

    added wireframe desing for index.html

--- run.py
+++ run.py
@@ -1,13 +1,35 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for, request
 
 """testing"""
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "Hello World from Globtopus with update"
+def index():
+    return render_template("index.html",
+                           )
+@app.route('/user')
+def user():
+    return render_template("user.html",
+                           )
+
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html',
+                          )
+
+@app.route('/sign_in')
+def sign_in():
+    return render_template('sign_in.html',
+                          )
+
+@app.route('/sign_up')
+def sign_up():
+    return render_template('sign_up.html',
+                          )
+
 
 
 """it should work"""
