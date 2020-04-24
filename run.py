588227301b7f1b3ce2106d975d3bfb1e6d43a4b5
Marcel Kolarcik <60097861+marcelkolarcik@users.noreply.github.com>
2020-04-24 01:46:34 +0100

    testing mongodb

--- run.py
+++ run.py
@@ -1,15 +1,37 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for, request
-
-
+import pymongo
+from dotenv import load_dotenv
+load_dotenv()
+MONGODB_URI = os.getenv('MONGO_URI')
+print('MONGODB_URI',MONGODB_URI)
+DBS_NAME = "globtopus_db"
+COLLECTION_NAME = "users"
 """testing"""
 app = Flask(__name__)
 
 
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print('connected', url)
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print('not connected %s') % e
+
+
+
 @app.route('/')
 def index():
+    conn = mongo_connect(MONGODB_URI)
+    coll = conn[DBS_NAME][COLLECTION_NAME]
+    documents = coll.find()
+    return render_template('index.html',
+                           documents=documents)
 
-    return render_template("index.html")
+
+
+
 @app.route('/user')
 def user():
     return render_template("user.html",
@@ -19,22 +41,24 @@ def user():
 @app.route('/admin')
 def admin():
     return render_template('admin.html',
-                          )
+                           )
+
 
 @app.route('/sign_in')
 def sign_in():
     return render_template('sign_in.html',
-                          )
+                           )
+
 
 @app.route('/sign_up')
 def sign_up():
     return render_template('sign_up.html',
-                          )
-
+                           )
 
 
 """it should work"""
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)
+    app.run(
+        port=os.environ.get('PORT'),
+        host=os.environ.get('IP'),
+        debug=True)
