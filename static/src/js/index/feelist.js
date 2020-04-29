007ee7c2611efab2486ad5197c9f6e4f7654fcf0
Marcel Kolarcik <60097861+marcelkolarcik@users.noreply.github.com>
2020-04-29 13:23:30 +0100

    remove test alert from feelist.js

--- static/src/js/index/feelist.js
+++ static/src/js/index/feelist.js
@@ -2,7 +2,6 @@
 
 $(function () {
     $('#feelist').on('click', function () {
-        alert('hello')
 
         /*AND FOR MOBILE DEVICES WE NEED TO SCROLL TO SEARCH RESULTS*/
         $('#feelist_search').toggleClass('d-none').get(0).scrollIntoView();
