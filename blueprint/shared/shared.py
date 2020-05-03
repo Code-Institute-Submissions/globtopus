89c0247e391357e34720b27770a58f9fb299e3e0
Marcel Kolarcik <60097861+marcelkolarcik@users.noreply.github.com>
2020-05-03 01:36:16 +0100

    removed blueprint forlder from repo

--- blueprint/shared/shared.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-shared_bp = Blueprint('shared_bp', __name__,
-                      template_folder='templates',
-                      static_folder='static',
-                      static_url_path='shared')
